8c743939cd281e053379b7412c8f4fa0e16c73d1
김휘수 <qkqhrkxdkdy@naver.com>
2024-07-20 15:01:46 +0900

    sql문 추가

---
+++ query.sql
@@ -0,0 +1,179 @@
+-- 관리자 테이블
+CREATE TABLE Admin (
+    a_id INT AUTO_INCREMENT PRIMARY KEY,
+    admin_id VARCHAR(50) NOT NULL,
+    admin_pw VARCHAR(50) NOT NULL,
+    admin_name VARCHAR(50) NOT NULL,
+    admin_phone VARCHAR(20) NOT NULL
+);
+
+-- 회원가입, 로그인, 내정보 테이블
+CREATE TABLE Users (
+    u_id INT AUTO_INCREMENT PRIMARY KEY,  
+    user_id VARCHAR(50) NOT NULL,
+    user_pw VARCHAR(50) NOT NULL,
+    user_name VARCHAR(50) NOT NULL,
+    user_phone VARCHAR(20) NOT NULL,
+    pet_name VARCHAR(50) NOT NULL,
+    pet_gender ENUM('Male', 'Female') NOT NULL, 
+    pet_neutering VARCHAR(20),
+    peculiarity VARCHAR(100)
+);
+
+-- 시설소개 테이블
+CREATE TABLE Facilities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    facility_name VARCHAR(50) NOT NULL,
+    main_facilities TEXT NOT NULL,
+    operating_hours VARCHAR(20) NOT NULL,
+    contact_info VARCHAR(50) NOT NULL, 
+    photo BLOB,
+    a_id INT,
+    FOREIGN KEY (a_id) REFERENCES Admin(a_id)
+); 
+
+-- 직원소개 테이블
+CREATE TABLE Staff (
+    staff_id INT AUTO_INCREMENT PRIMARY KEY,  
+    name VARCHAR(100) NOT NULL, 
+    role VARCHAR(50),
+    contact_info VARCHAR(100),
+    a_id INT,
+    FOREIGN KEY (a_id) REFERENCES Admin(a_id)
+);
+
+CREATE TABLE Dogs (
+    dog_id INT PRIMARY KEY AUTO_INCREMENT,
+    dog_photo BLOB,
+    pet_name VARCHAR(15) NOT NULL,
+    owner_name VARCHAR(15) NOT NULL,
+    walk_date DATE NOT NULL,
+    walk_time TIME NOT NULL,
+    walk_photo BLOB ,
+    teacher VARCHAR(15) NOT NULL,
+    class_info VARCHAR(15) NOT null,
+    note_info TEXT,
+    feed BOOLEAN NOT NULL
+);
+
+insert into dogs (pet_name, owner_name, walk_date, walk_time, teacher, class_info, feed)
+  values ("puppy", "aaa", "2024-07-07", "12:10", "t", "morning", true);
+  
+insert into dogs (pet_name, owner_name, walk_date, walk_time, teacher, class_info, feed)
+  values ("puppy1", "aaa", "2024-07-17", "12:10", "t", "afternoon", true); 
+  
+-- 수업시간표 테이블 (오전)
+CREATE TABLE MorningClassSchedule (
+    schedule_id INT AUTO_INCREMENT PRIMARY KEY,  
+    class_type ENUM('오전') NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description VARCHAR(255),
+    a_id INT,
+    FOREIGN KEY (a_id) REFERENCES Admin(a_id)
+);
+
+-- 수업시간표 테이블 (오후)
+CREATE TABLE AfternoonClassSchedule (
+    schedule_id INT AUTO_INCREMENT PRIMARY KEY,  
+    class_type ENUM('오후') NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description VARCHAR(255),
+    a_id INT,
+    FOREIGN KEY (a_id) REFERENCES Admin(a_id)
+);
+
+-- 수업시간표 테이블 (종일)
+CREATE TABLE AlldayClassSchedule (
+    schedule_id INT AUTO_INCREMENT PRIMARY KEY,  
+    class_type ENUM('종일') NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description VARCHAR(255),
+    a_id INT,
+    FOREIGN KEY (a_id) REFERENCES Admin(a_id)
+);
+
+-- 공지사항 테이블
+CREATE TABLE NoticeBoard (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    image BLOB,
+    createBy VARCHAR(50) NOT NULL,
+    a_id INT,
+    FOREIGN KEY (a_id) REFERENCES Admin(a_id)
+);
+
+-- 자유게시판 테이블
+CREATE TABLE FreeBoard (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    image BLOB,
+    a_id INT,
+    FOREIGN KEY (a_id) REFERENCES Admin(a_id)
+);
+
+-- QnA게시판 테이블
+CREATE TABLE QnaBoard (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    question_title VARCHAR(255) NOT NULL,
+    question_content TEXT NOT NULL,
+    answer_content TEXT,
+    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    answer_date TIMESTAMP,
+    a_id INT,
+    FOREIGN KEY (a_id) REFERENCES Admin(a_id)
+);
+
+-- 펫 추가 테이블
+CREATE TABLE Pets (
+    pet_id INT AUTO_INCREMENT PRIMARY KEY,
+    pet_name VARCHAR(50) NOT NULL,
+    pet_gender ENUM('Male', 'Female') NOT NULL, 
+    pet_neutering VARCHAR(20),
+    peculiarity VARCHAR(100),
+    u_id INT,  
+    FOREIGN KEY (u_id) REFERENCES Users(u_id)
+  );
+  
+  -- 수업등록 테이블( 실패 )
+CREATE TABLE ClassRegistration (
+    registration_id INT AUTO_INCREMENT PRIMARY KEY,  
+    dog_id INT NOT NULL, 
+    owner_id INT NOT NULL,  
+    class_date DATE NOT NULL,
+    schedule_id INT NOT NULL,  
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    a_id INT,
+    FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id),
+    FOREIGN KEY (owner_id) REFERENCES Users(user_id),
+    FOREIGN KEY (schedule_id) REFERENCES MorningClassSchedule(schedule_id),
+    FOREIGN KEY (schedule_id) REFERENCES AfternoonClassSchedule(schedule_id),
+    FOREIGN KEY (schedule_id) REFERENCES AlldayClassSchedule(schedule_id),
+    FOREIGN KEY (a_id) REFERENCES Admin(a_id)
+);
+
+--수업 신청 테이블
+CREATE TABLE ClassRegistration (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_name VARCHAR(100) NOT NULL,
+    dog_name VARCHAR(100) NOT NULL,
+    class_name VARCHAR(100) NOT NULL,
+    feed_status BOOLEAN NOT NULL,
+    pickup_status BOOLEAN NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    consultation TEXT, -- 관리자용
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    admin_id INT,
+    FOREIGN KEY (admin_id) REFERENCES Admin(a_id)
+);
+
