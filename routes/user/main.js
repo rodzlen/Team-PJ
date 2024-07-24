614e1f7e5322fc63d5de0ac2e2e48f79f5694d90
김휘수 <qkqhrkxdkdy@naver.com>
2024-07-25 01:22:17 +0900

    user 검색기능 완료

--- routes/user/main.js
+++ routes/user/main.js
@@ -49,7 +49,7 @@ router.get(
     const searchQuery = req.query.search || "";
     const typeQuery = req.query.type || "";
 
-    let query = "SELECT * FROM noticeBoard";
+    let query = "SELECT * FROM noticeboard";
     let queryParams = [];
 
     if (searchQuery) {
@@ -66,6 +66,7 @@ router.get(
     }
 
 
+
     db.query(query, queryParams, (err, results) => {
       if (err) {
         console.error(err);
@@ -74,6 +75,8 @@ router.get(
         res.render("user/notice/user_notice_main", {
           locals,
           data: results,
+          searchQuery,
+          typeQuery,
           layout: mainLayout,
         });
       }
@@ -369,7 +372,7 @@ router.post(
   })
 );
 
-//qna 목록
+// qna 목록
 router.get(
   "/qna",
   asyncHandler(async (req, res) => {
@@ -379,7 +382,19 @@ router.get(
     const searchQuery = req.query.search || "";
     const typeQuery = req.query.type || "";
     let queryParams = [];
-    search(query, searchQuery, typeQuery);
+    if (searchQuery) {
+      if (typeQuery === "title") {
+        query += " WHERE q.title LIKE ?";
+        queryParams.push(`%${searchQuery}%`);
+      } else if (typeQuery === "createBy") {
+        query += " WHERE q.question_by LIKE ?";
+        queryParams.push(`%${searchQuery}%`);
+      } else if (typeQuery === "title||createBy") {
+        query += " WHERE q.title LIKE ? OR q.question_by LIKE ?";
+        queryParams.push(`%${searchQuery}%`, `%${searchQuery}%`);
+      }
+    }
+
     db.query(query, queryParams, (err, results) => {
       if (err) {
         console.error(err);
@@ -388,6 +403,8 @@ router.get(
         res.render("user/qna/user_qna_main", {
           locals,
           data: results,
+          searchQuery,
+          typeQuery,
           layout: mainLayout,
         });
       }
